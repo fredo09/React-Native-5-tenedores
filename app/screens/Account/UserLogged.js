e6c04943d6526eee2aae7236846a30eb867d14c8
Fredy <desaMts1806@MacBook-Pro-de-macpro.local>
2020-01-16 18:20:15 -0600

    Agengando funcionalidad para el cambio de avatar por medido de expo-permissions, expo-Image-picker y firebase, ademas de actualizar en tiempo real y agregando el loading y toast

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -2,22 +2,54 @@
 *   User Logged 
 */
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React,{ useState, useEffect, useRef } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
+import { UserInfo } from './../../components/AccountForms/InfoUser';
+import { Loading } from './../../components/Loading';
 import * as Firebase from 'firebase';
+import Toast from 'react-native-easy-toast';
 
 export const UserLogged = () => {
+    
+    //States
+    const [userInfo, setUserInfo] = useState({})
+    const [reloadData, setReloadData] = useState(false);
+    const [isVisibleLoading, setIsVisibleLoading] = useState(false);
+    const [textInfo, setTextInfo] = useState('');
+    const toastRef = useRef();
+
+    useEffect(() => {
+        (async() => {
+            const user = await Firebase.auth().currentUser;
+            setUserInfo(user.providerData[0]);
+        })();
+        setReloadData(false);
+    },[reloadData]);
+
     return(
         <View>
-            <Text>
-                Usuario Logeado!
-            </Text>
+            <UserInfo 
+                userInfo={userInfo} 
+                setReloadData={setReloadData} 
+                toastRef={toastRef}
+                setIsVisibleLoading={setIsVisibleLoading}
+                setTextInfo={setTextInfo}
+            />
             <Button 
                 title="Logout"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btnstyle}
                 onPress={() => { Firebase.auth().signOut() }}
+            />
+            <Toast 
+                position="center"
+                opacity={0.5}
+                ref={toastRef}
+            />
+            <Loading 
+                isVisible={isVisibleLoading}
+                text={textInfo}
             />
         </View>
     )
@@ -25,7 +57,7 @@ export const UserLogged = () => {
 
 const styles = StyleSheet.create({
     btnContainer:{
-        width: "90%"
+        width: "100%"
     },
     btnstyle:{
         backgroundColor: "#00a680"
